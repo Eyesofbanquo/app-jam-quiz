e3c803112cd3325cfc6bccbea010d87da6fbce2f
Markim Shaw <ms79723@gmail.com>
2020-09-19 12:57:09 -0500

    Initial Commit

---
+++ app-jam-quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-jam-quiz
+//
+//  Created by Markim Shaw on 9/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-jam-quiz/app_jam_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_jam_quizApp.swift
+//  app-jam-quiz
+//
+//  Created by Markim Shaw on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct app_jam_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
